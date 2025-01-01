3fc281c3e2be1b43e65916f1e8b98c8038dd5b1d
homval <in00@list.ru>
2025-01-01 22:44:37 +0300

    add jdbcAuthentication security

--- src/main/java/ru/example/tacocloud/config/SecurityConfig.java
+++ src/main/java/ru/example/tacocloud/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.example.tacocloud.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,22 +9,41 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        String passwordBuzz = passwordEncoder().encode("buzz-pass");
+//        String passwordFrog = passwordEncoder().encode("frog-pass");
+//        auth.inMemoryAuthentication()
+//                .withUser("buzz")
+//                .password(passwordBuzz)
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("frog")
+//                .password(passwordFrog)
+//                .authorities("ROLE_USER");
+//
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        String passwordBuzz = passwordEncoder().encode("buzz-pass");
-        String passwordFrog = passwordEncoder().encode("frog-pass");
-        auth.inMemoryAuthentication()
-                .withUser("buzz")
-                .password(passwordBuzz)
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("frog")
-                .password(passwordFrog)
-                .authorities("ROLE_USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username, password, enabled from Users where username=?"
+                )
+                .authoritiesByUsernameQuery(
+                        "select username, authority from UserAuthorities where username=?"
+                )
+                .passwordEncoder(passwordEncoder());
 
     }
 
